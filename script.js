b5226006af1043a30889d1ca42d44a50d699a3b3
abdurrahman <rajab-2@windowslive.com>
2019-11-03 01:10:26 +0300

    V0.01 Dev
    Add Apps base files,
    Add arabic data
    Write data reading script
    Add HTML base templates,

---
+++ script.js
@@ -0,0 +1,76 @@
+/**
+ * The base url of the data from project to read them.
+ * 
+ */
+let dataUrl = "https://a0m0rajab.github.io/BahisQurani/data/"
+    /**
+     * Data JSON to read them from it.
+     * Need to add: Translation for other languages.
+     */
+let data = {
+        arclean: "quran-simple-clean.txt",
+        aruthman: "quran-uthmani.txt",
+
+    }
+    /**
+     * Arabic sura array hold nested array of suras and aya,
+     * This is the base array of Quran with uthmani font. 
+     * suraAr[X][Y] => x number of sura, Y number of aya
+     */
+let suraAr = []
+
+/**
+ * Same as SuraAr but this one does not use uthmani font or even diactrics(harakat) to simplify search methods
+ * @see suraAr
+ */
+let suraSr = []
+    /**
+     * 
+     * Reading data from external source, the send it to a local callback funtion to use it.
+     * 
+     * @param {*} url The data Url
+     * @param {*} target The target array to hold the text
+     * @param {*} callBack The call back function to read and parse the file to array.
+     * @see dataToArray
+     */
+function readExternal(url, target, callBack) {
+    console.log("reading external data from " + url)
+    fetch(url)
+        .then(r => r.text()) //response
+        .then(t => callBack(t, target)); //text
+}
+
+/**
+ * 
+ * File parsing function to parse the responce from external URL.
+ * 
+ * @param {String} t text from responce
+ * @param {Array} targetArray Target array to send the modified data to it.
+ * @see readExternal
+ */
+function dataToArray(t, targetArray, ) {
+    // split the file to line, which downloaded from Tanzil.
+    let lines = t.split("\n");
+    let suraN;
+    console.log("Quran Verses= " + lines.length);
+    // temp to hold the number of sura, so we can use it to trigger the next sura and empty the verses array.
+    let versesArray = [];
+    for (let line of lines) {
+        line = line.split("|");
+        suraN = line[0] - 1;
+        if (targetArray[suraN] == undefined) {
+            targetArray[suraN] = [];
+        }
+        targetArray[suraN].push(line[2])
+    }
+}
+/**
+ * Set Arabic arrays and initialize it.
+ */
+function setArabic() {
+    // set uthmani array.
+    readExternal(dataUrl + data.aruthman, suraAr, dataToArray)
+        // set clean array.
+    readExternal(dataUrl + data.arclean, suraSr, dataToArray)
+
+}
