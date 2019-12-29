f9d6af77403cd3349df1f896be9a535cf0cc3c80
abdurrahman <rajab-2@windowslive.com>
2019-12-29 22:47:45 +0300

    SW test - hope it will work.

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@ const FILES = [
   '/BahisQurani/data/tr.diyanet.txt',
   '/BahisQurani/data/tr.yazir.txt',
   '/BahisQurani/style/me_quran.ttf',
-  '/BahisQurani/manifest.json',
+  'BahisQurani/manifest.json',
 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
 'https://code.jquery.com/jquery-3.3.1.slim.min.js',
 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
