240ee5446dbd90e22dd9582b30e1c5f182be06f7
abdurrahman rajab <rajab-2@windowslive.com>
2020-07-05 10:10:06 +0300

    Add index page
    Add index page for the whole website.

--- code/config.js
+++ code/config.js
@@ -7,7 +7,7 @@ let config = {
     finder: "Ara",
     reader: "Oku",
     similarity: "simi",
-    version: "V2.06",
+    version: "V2.07",
     author: "Abdurrahman Rajab",
     gitHub: "a0m0rajab",
     idea: "Mehmet Akif Eyler"
