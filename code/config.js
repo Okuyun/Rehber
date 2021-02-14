cc4c577493d4d4b212586e59bf914775e429a9f4
Abdurrahman Rajab <rajab-2@windowslive.com>
2021-02-14 11:50:48 +0300

    fix(view):using more of the screen. Changing the screen max width to have more text on screen. Fixing highlight cover Adding more space between lines, show better results.

--- code/config.js
+++ code/config.js
@@ -7,7 +7,7 @@ let config = {
     finder: "Ara",
     reader: "Oku",
     similarity: "simi",
-    version: "V2.2",
+    version: "V2.21",
     author: "Abdurrahman Rajab",
     gitHub: "a0m0rajab",
     idea: "Mehmet Akif Eyler"
